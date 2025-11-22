4c5f8e8e59839a206e5c60495d5f573afed39a2e
xxyzyh-code <xxyzyh@gmail.com>
2025-11-22 19:03:13 +0800

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+import { initializeDOMElements } from './Config.js';
+import './StateAndUtils.js';
+import './LrcParser.js';
+import './AudioEngine.js';
+import './PlayerCore.js';
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeDOMElements();
+});
